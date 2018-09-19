8a92c118312e345982759d49c0b592c796fc18be
Kamesas <semakaleksandr2014@gmail.com>
2018-09-19 11:17:07 +0300

    all work !

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Card({ book }) {
   return (
@@ -16,7 +17,7 @@ function Card({ book }) {
       </div>
 
       <section>
-        <a href={`/books/${book.topic}/${book.slug}`}>Подробнее</a>
+        <Link to={`/books/${book.topic}/${book.slug}`}>Подробнее</Link>
       </section>
     </div>
   );
