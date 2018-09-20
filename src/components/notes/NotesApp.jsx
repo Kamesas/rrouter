0df81c5949abbfcb16419dee78f25091d2ac2c42
Kamesas <semakaleksandr2014@gmail.com>
2018-09-20 14:51:01 +0300

    componentDidUpdate

--- src/components/notes/NotesApp.jsx
+++ src/components/notes/NotesApp.jsx
@@ -15,10 +15,13 @@ class NotesApp extends Component {
   };
 
   handleNoteApp = newNote => {
-    this.setState(
-      { notes: [newNote, ...this.state.notes] },
-      this.saveToLocalStorage
-    );
+    this.setState({ notes: [newNote, ...this.state.notes] });
+  };
+
+  handleNoteDelete = noteId => {
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== noteId)
+    });
   };
 
   saveToLocalStorage = () => {
@@ -26,19 +29,16 @@ class NotesApp extends Component {
     localStorage.setItem("notes", notes);
   };
 
-  handleNoteDelete = noteId => {
-    this.setState(
-      {
-        notes: this.state.notes.filter(note => note.id !== noteId)
-      },
-      this.saveToLocalStorage
-    );
-  };
-
   componentDidMount() {
     const savedNotes = JSON.parse(localStorage.getItem("notes"));
     if (savedNotes) {
       this.setState({ notes: savedNotes });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.notes !== this.state.notes) {
+      this.saveToLocalStorage();
     }
   }
 
