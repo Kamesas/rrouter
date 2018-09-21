6b5dc272e45070eea41776abb8693327878297a7
Kamesas <semakaleksandr2014@gmail.com>
2018-09-21 11:16:41 +0300

    some change

--- src/components/notes/notesApp/HOC/PassProps.jsx
+++ src/components/notes/notesApp/HOC/PassProps.jsx
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 
 export default function PassProps(props = {}, WrapComponent) {
   class EnhancedComponent extends Component {
-    state = {};
     render() {
-      console.log(this.props);
       return <WrapComponent {...props} {...this.props} />;
     }
   }
