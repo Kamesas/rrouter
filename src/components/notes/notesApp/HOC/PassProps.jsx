370c71e4a05ede8a185734e7c0133076e717bf78
Kamesas <semakaleksandr2014@gmail.com>
2018-09-21 10:04:45 +0300

    add HOC

--- src/components/notes/notesApp/HOC/PassProps.jsx
+++ src/components/notes/notesApp/HOC/PassProps.jsx
@@ -4,6 +4,7 @@ export default function PassProps(props = {}, WrapComponent) {
   class EnhancedComponent extends Component {
     state = {};
     render() {
+      console.log(this.props);
       return <WrapComponent {...props} {...this.props} />;
     }
   }
