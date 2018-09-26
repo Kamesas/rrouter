23c855f5d2b2a647788bb41cb8ea69b534453ced
Kamesas <semakaleksandr2014@gmail.com>
2018-09-26 17:31:58 +0300

    got  tasks from store

--- src/components/toDoApp/components/ToDoList.jsx
+++ src/components/toDoApp/components/ToDoList.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import ToDo from "./ToDo";
+import { connect } from "react-redux";
 
-export default class TodoList extends Component {
+class TodoList extends Component {
   render() {
     return (
       <div className="toDoList">
-        <ToDo />
+        {this.props.todos.map(task => (
+          <ToDo key={task.id} text={task.text} />
+        ))}
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { todos: state.ToDoAppTask };
+}
+
+export default connect(mapStateToProps)(TodoList);
